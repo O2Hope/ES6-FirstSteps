86be2a5a09ea6918526a85347da3877106621348
Agustin <agustin@localhost.localdomain>
2018-03-06 23:04:15 -0600

    Arrow function Basics

--- script.js
+++ script.js
@@ -71,3 +71,31 @@ console.log(n.startsWith('A'));
 console.log(n.endsWith('iz'));
 console.log(n.includes(' '));
 console.log(`${firstName} `.repeat(5));
+
+///////////////////////////////////////////////////////////////
+//Arrow Functions
+
+const years = [1992,1998,1996,1920];
+
+//ES5
+var ages5 = years.map(function (el) {
+    return 2016 - el;
+});
+
+console.log(ages5);
+
+//ES6
+
+let ages6 = years.map(el => 2018 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`;
+});
+
+console.log(ages6);
